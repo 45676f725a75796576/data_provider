55a1762b4b9ae671c854565b0d217028aef1aeff
45676f725a75796576 <egor.zuyev33@gmail.com>
2026-01-18 13:44:36 +0100

    added compatibility with MySQL

--- data_provider.py
+++ data_provider.py
@@ -1,9 +1,6 @@
 import db_data_provider
 
 from os import getenv
-import pyodbc
-
-import re
 
 __connection = None
 
@@ -14,10 +11,16 @@ connection_dict = {
     'pwd': getenv('PWD')
 }
 
-def set_connection_data(**kwargs):
+def set_connection_data(connection_data: dict):
+    """Sets data for connection.
+
+    Args:
+        connection_data (dict): Must contain 'uid' - username, 'pwd' - password, 'server' - server address, 'database' - database on server
+    """
+    
     global connection_dict
     
-    connection_dict = kwargs
+    connection_dict = connection_data
 
 def request_rows_by_value_in_column(table: str, column: str, value: int) -> list[dict]:
     """Returns rows, where value of column in parameters is same as value parameter.\n   
@@ -31,28 +34,28 @@ def request_rows_by_value_in_column(table: str, column: str, value: int) -> list
     Returns:
         list[dict]: Returns list of rows as dict with saved column names.
     """
-    
-    if not __connection:
-        raise RuntimeError('Database is not connected.')
-    else:
+    if connection_dict['driver'] != None:
         pr = db_data_provider.msssql_data_provider()
-        pr.connect(connection_dict)
+    else:
+        pr = db_data_provider.mysql_data_provider()
+    pr.connect(connection_dict)
         
-        return pr.request_row_by_value_in_column(table, column, value)
+    return pr.request_row_by_value_in_column(table, column, value)
     
 def set_value_in_column_by_id(table: str, column: str, id: str, value: str):
-    if not __connection:
-        raise RuntimeError('Database is not connected.')
-    else:
+    if connection_dict['driver'] != None:
         pr = db_data_provider.msssql_data_provider()
-        pr.connect(connection_dict)
-        pr.set_value_in_column_by_id(table, column, id, value)
+    else:
+        pr = db_data_provider.mysql_data_provider()
+    
+    pr.connect(connection_dict)
+    pr.set_value_in_column_by_id(table, column, id, value)
 
 def get_value_in_column_by_id(table: str, column: str, id: str):
-    if not __connection:
-        raise RuntimeError('Database is not connected.')
-    else:
+    if connection_dict['driver'] != None:
         pr = db_data_provider.msssql_data_provider()
-        pr.connect(connection_dict)
-        pr.get_value_in_column_by_id(table, column, id)
+    else:
+        pr = db_data_provider.mysql_data_provider()
+    pr.connect(connection_dict)
+    pr.get_value_in_column_by_id(table, column, id)
 
--- db_data_provider.py
+++ db_data_provider.py
@@ -1,12 +1,16 @@
 import re
 import pyodbc
+import mysql.connector
 
 class msssql_data_provider:
     """Data provider for Microsoft SQL Server
     """
     __connection = None
+    
+    def __init__(self):
+        pass
 
-    def request_row_by_value_in_column(table: str, column: str, value: int) -> list[dict]:
+    def request_row_by_value_in_column(self, table: str, column: str, value: int) -> list[dict]:
         """Returns rows, where value of column in parameters is same as value parameter.\n
         After executing this method, connection closes.
 
@@ -19,9 +23,12 @@ class msssql_data_provider:
             list[dict]: Returns list of rows as dict with saved column names
         """
         
+        if self.__connection == None:
+            raise RuntimeError("No connection to DB.")
+        
         if re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', table) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', column) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', value):
             raise ValueError("Parameters cannot contain special symbols.")
-        cxr = __connection.cursor()
+        cxr = self.__connection.cursor()
         cxr.execute(f'SELECT * FROM {table} WHERE {column} = ?;', (value,))
 
         rows = cxr.fetchall()
@@ -31,30 +38,36 @@ class msssql_data_provider:
         for row in rows:
             l.append(dict(zip(columns, row)))
             
-        __connection.close()
+        self.__connection.close()
             
         return l
     
-    def get_value_in_column_by_id(table: str, column: str, id: str) -> tuple:
+    def get_value_in_column_by_id(self, table: str, column: str, id: str) -> tuple:
+        if self.__connection == None:
+            raise RuntimeError("No connection to DB.")
+        
         if re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', table) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', column) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', id):
             raise ValueError("Parameters cannot contain special symbols.")
-        cxr = __connection.cursor()
+        cxr = self.__connection.cursor()
         cxr.execute(f'SELECT {column} FROM {table} WHERE id = ?', (id,))
         
         row = cxr.fetchone()
-        __connection.close()
+        self.__connection.close()
         
         return row[0] if row else None
         
 
-    def set_value_in_column_by_id(table: str, column: str, id: str, value: str):
+    def set_value_in_column_by_id(self, table: str, column: str, id: str, value: str):
+        if self.__connection == None:
+            raise RuntimeError("No connection to DB.")
+        
         if re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', table) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', column) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', id) or re.fullmatch('^[A-Za-z_][A-Za-z0-9_]*$', value):
             raise ValueError("Parameters cannot contain special symbols.")
-        cxr = __connection.cursor()
+        cxr = self.__connection.cursor()
         cxr.execute(f'UPDATE {table} SET {column} = ? WHERE id = ?', (value, id,))
-        __connection.close()
+        self.__connection.close()
 
-    def connect(conn_data: dict):
+    def connect(self, conn_data: dict):
         """This method must be executed before every request.
 
         Args:
@@ -63,7 +76,6 @@ class msssql_data_provider:
         Raises:
             ValueError: Raises exception if data is missing
         """
-        global __connection
 
         driver = conn_data['driver']
         server = conn_data['server']
@@ -74,6 +86,61 @@ class msssql_data_provider:
         if not driver or not server or not database or not uid or not pwd:
             raise ValueError('Missing values.')
 
-        __connection = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={uid};PWD={pwd}')
+        self.__connection = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={uid};PWD={pwd}')
+        
+class mysql_data_provider:
+    
+    __connection = None
+    
+    def __init__(self):
+        pass
+    
+    def request_row_by_value_in_column(self, table: str, column: str, value: int) -> list[dict]:
+        if self.__connection == None:
+            raise RuntimeError("No connection to DB.")
+        cxr = self.__connection.cursor(buffered=True, dictionary=True)
+        cxr.execute(f'SELECT * FROM {table} WHERE {column} = {value}')
+        
+        r = cxr.fetchall()
+        
+        self.__connection.close()
+        
+        return r
+    
+    def set_value_in_column_by_id(self, table: str, column: str, id: str, value: str):
+        if self.__connection == None:
+            raise RuntimeError("No connection to DB.")
+        cxr = self.__connection.cursor(buffered=True)
+        cxr.execute(f'UPDATE {table} SET {column} = {value} WHERE id = {id}')
+        
+        self.__connection.close()
+    
+    def get_value_in_column_by_id(self, table: str, column: str, id: str) -> tuple:
+        if self.__connection == None:
+            raise RuntimeError("No connection to DB.")
+        
+        cxr = self.__connection.cursor(buffered=True, dictionary=True)
+        cxr.execute(f'SELECT {column} FROM {table} WHERE id = {id}')
+        r = cxr.fetchone()
+        
+        self.__connection.close()
+        
+        for k, v in r.items():
+            return (k, v)
+    
+    def connect(self, conn_data: dict):
+        """This method must be executed before every request.
+
+        Args:
+            conn_data (dict): Connection data, must contain parameters 'server', 'database', 'uid', 'pwd'
+
+        Raises:
+            ValueError: Raises exception if data are missing
+        """
+        
+        if not conn_data['server'] or not conn_data['database'] or not conn_data['uid'] or not conn_data['pwd']:
+            raise ValueError('Missing data.')
+        
+        self.__connection = mysql.connector.connect(host=conn_data['server'] ,user=conn_data['uid'], password=conn_data['pwd'], database=conn_data['database'])
     
     
